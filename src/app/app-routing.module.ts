74ff86fa0c862de3774a69d37d647806a74225b6
ThomasTritscher <thomas.tritscher33@googlemail.com>
2021-04-09 10:09:45 +0200

    added data protection site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 
@@ -8,7 +9,9 @@ import { SlideshowComponent } from './slideshow/slideshow.component';
 
 const routes: Routes = [
   { path: '', component: SlideshowComponent},
-  { path: 'imprint' , component: ImprintComponent }
+  { path: 'imprint' , component: ImprintComponent },
+  { path: 'data-protection' , component: DataProtectionComponent }
+
 ];
 
 @NgModule({
