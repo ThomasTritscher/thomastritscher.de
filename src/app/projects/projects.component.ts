d019e1663e257c538a0da461a71e8d0de1fc3aae
ThomasTritscher <thomas.tritscher33@googlemail.com>
2021-06-01 17:41:50 +0200

    added scroll function

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,34 +1,28 @@
 import { getTranslationDeclStmts } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
   translate = 'translate3d(0px, 0%, 0px)';
+  offSet = 0;
   constructor() { }
-
   ngOnInit(): void {
     window.addEventListener("wheel", (event: any) => {
+      console.log("SCROLLING");
       if (event.deltaY < 0) {
         console.log('scrolling up');
-
-
-
+        this.offSet++;
       } else if (event.deltaY > 0) {
         console.log('scrolling down');
-        scroll();
-
-
+        this.offSet--;
       }
-
+      this.scroll();
     });
   }
   scroll() {
-    this.translate = 'translate3d(0px, 50%, 0px)';
+    this.translate = `translate3d(0px, ${this.offSet}%, 0px)`;
   }
-
 }
